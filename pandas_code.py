ed403f7c2d9e2e7a4677b9e5494e8ac4dc3756ad
Muiz Junaid <muizjunaid@student.unsw.edu.au>
2025-05-22 22:04:23 +1000

    Updated code

--- pandas_code.py
+++ pandas_code.py
@@ -49,6 +49,13 @@ df2 = pd.concat([home_pts, away_pts], axis=1)
 df2['TotalPoints'] = df2['TotalHomePoints'] + df2['TotalAwayPoints']
 df2 = df2.head(6)
 
+
+
+
+'''
+Calculating Points per season
+'''
+
 #Plotting df2
 #df2.plot(kind='bar', title='Big 6 Points Summary', xlabel='Teams', ylabel='Points')
 #plt.show()
@@ -118,6 +125,11 @@ df4 = df4.loc[big_6]
 
 
 '''
+Getting number of losses per season
+'''
+
+
+'''
 GETTING NUMBER OF WINS DRAWS AND LOSSES AS A PERCENT OF GAMES
 '''
 
@@ -132,11 +144,10 @@ df4['LossPercentage'] = (df4['TotalLosses']/df4['TotalGamesPlayed']) * 100
 
 df5 = df3[['WinPercentage', 'DrawPercentage']].copy()
 df5['LossPercentage'] = df4['LossPercentage']
+df5['WinPercentage']= df5['WinPercentage'].sort_values(ascending=False)
 
-
-#df5.plot(kind='bar', legend='true', title='Big 6 Win Loss Draw Percentages', xlabel='Teams', ylabel='Percentage')
-#plt.show()
-
+df5.plot(kind='bar', legend='true', title='Big 6 Win Loss Draw Percentages', xlabel='Teams', ylabel='Percentage')
+plt.show()
 
 
 '''
@@ -159,10 +170,45 @@ df6['TotalPoints'] = df6['TotalHomePoints'] + df6['TotalAwayPoints']
 df6 = df6.reset_index()
 df6 = df6.fillna(0)
 
+
 pivot_table = df6[df6['Team'].isin(big_6)].pivot_table(index='Season', columns='Team', values='TotalPoints')
 pivot_table.fillna(0, inplace=True)
-print(type(pivot_table))
-pivot_table.plot(marker= 'o', title='Big 6 Points over seasons', xlabel='Year', ylabel='Points')
-print(pivot_table)
+pivot_table[['Man United', 'Man City']].plot(marker= 'o', title='Man Utd & Man City Points Over the Years', xlabel='Year', ylabel='Points')
 
-plt.show()
+
+
+#Average points for teams in between years 1993-2003, 2003-2013, 2013-2023
+period_1 = [
+    "1993-1994", "1994-1995", "1995-1996", "1996-1997", "1997-1998",
+    "1998-1999", "1999-2000", "2000-2001", "2001-2002", "2002-2003"
+]
+
+period_2 = [
+    "2003-2004", "2004-2005", "2005-2006", "2006-2007", "2007-2008",
+    "2008-2009", "2009-2010", "2010-2011", "2011-2012", "2012-2013"
+]
+
+period_3 = [
+    "2013-2014", "2014-2015", "2015-2016", "2016-2017", "2017-2018",
+    "2018-2019", "2019-2020", "2020-2021", "2021-2022", "2022-2023",
+    "2023-2024"
+]
+
+Overall = []
+new_df = pivot_table.reset_index()
+
+
+new_df_1 = new_df[new_df['Season'].isin(period_1)]
+new_df_2 = new_df[new_df['Season'].isin(period_2)]
+new_df_3 = new_df[new_df['Season'].isin(period_3)]    
+average_pts = []
+val = new_df_1['Man United'].mean()
+val2 = new_df_2['Man United'].mean()
+val3 = new_df_3['Man United'].mean()
+print(val, val2, val3)
+
+
+
+'''
+Finding which club finished First in each Pl season
+'''
